99e4a31b95d2486a89f214c387a6f784af804d9f
BrandonEscobar21 <brandonparedesacb2020@gmail.com>
2025-05-28 16:12:57 -0600

    Brandon Escobar: Metodos y Atributos de Freelancer creados

--- src/Freelancer.java
+++ src/Freelancer.java
@@ -1,2 +1,32 @@
-public class Freelancer {
+public abstract class Freelancer {
+    private int Id;
+    private String Nombre;
+    private double HorasTrabajadas;
+
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public double getHorasTrabajadas() {
+        return HorasTrabajadas;
+    }
+    public void setHorasTrabajadas(double horasTrabajadas) {
+        HorasTrabajadas = horasTrabajadas;
+    }
+
+
+    public abstract void CalcularPago();
+    public void MostrarInformaccion(){
+        
+    }
 }
