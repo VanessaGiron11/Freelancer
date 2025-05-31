8916e954deb30a82bd307e5b6c063f9c5aef572f
VanessaGiron11 <hubgit079@gmail.com>
2025-05-31 17:30:59 -0600

    Clase hija ProgramadorFreelancer creada t proframada

--- src/ProgramadorFreelance.java
+++ src/ProgramadorFreelance.java
@@ -1,2 +1,28 @@
-public class ProgramadorFreelance {
+public class ProgramadorFreelance extends Freelancer{
+    private String Nombre;
+    private Double Salario;
+
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public Double getalario() {
+        return Salario;
+    }
+    public void setSalario(Double salario) { Salario = salario;}
+
+    @Override
+    public double CalcularPago()
+    { return HorasTrabajadas * Salario;}
+
+    @Override
+    public void MostrarInformaccion() {
+        System.out.println("Nombre: " + Nombre);
+        System.out.println("Id: " + Id);
+        System.out.println("Horas trabajadas: " + HorasTrabajadas);
+        System.out.println("El total del pago es: " + Salario);
+    }
 }
